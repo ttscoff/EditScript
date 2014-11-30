79da7ae71a4023a35be016005e11962b9807ee21
Brett Terpstra <me@brettterpstra.com>
2014-11-29 18:41:53 -0600

    oops

--- test/otask_test.rb
+++
@@ -1,9 +0,0 @@
-require "minitest/autorun"
-require "otask"
-
-class TestOTask < MiniTest::Unit::TestCase
-  def test_parsed_options_returns_true_for_valid_arguments
-    task = OTask.new(["-g"], '')
-    assert_equal true, task.send(:parsed_options?)
-  end
-end
